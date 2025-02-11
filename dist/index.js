d1597745fb0e4f3bf1f11df46b9f0c15d25966ba
WSD <wahyu.siddarta97@gmail.com>
2025-02-11 10:05:16 +0800

    refactor: add CORS support and re-enable compression middleware with custom filter

--- dist/index.js
+++ dist/index.js
@@ -16,13 +16,19 @@ const express_1 = __importDefault(require("express"));
 const postgres_1 = __importDefault(require("./db/postgres"));
 const authRoutes_1 = __importDefault(require("./routes/authRoutes"));
 const StockRoutes_1 = __importDefault(require("./routes/StockRoutes"));
-// import compression from "compression"; // Import the compression middleware
+const compression_1 = __importDefault(require("compression")); // Import the compression middleware
 const middleware_1 = require("./middleware");
+const cors_1 = __importDefault(require("cors"));
 const logger_1 = __importDefault(require("./logger"));
 const app = (0, express_1.default)();
 const port = 3000;
 const hostName = "0.0.0.0";
 app.use(express_1.default.json());
+app.use((0, cors_1.default)({
+    origin: "*", // Allow all origins (change this for security)
+    methods: "GET,POST,PUT,DELETE",
+    allowedHeaders: "Content-Type, Authorization",
+}));
 app.use("/api", middleware_1.apiLimiter); // Apply rate limiter to all /api routes
 app.get("/api/ping", (req, res) => {
     res.send("pong");
@@ -36,16 +42,14 @@ const startServer = () => __awaiter(void 0, void 0, void 0, function* () {
             app.use("/api/public", authRoutes_1.default);
             // Apply JWT middleware and prefix /private to stockRoutes
             app.use("/api/private", middleware_1.authenticateJWT, StockRoutes_1.default);
-            // app.use(
-            //   compression({
-            //     filter: (req, res) => {
-            //       if (req.headers["x-no-compression"]) {
-            //         return false; // Bypass compression if the client requests it
-            //       }
-            //       return compression.filter(req, res); // Use default filter
-            //     },
-            //   })
-            // );
+            app.use((0, compression_1.default)({
+                filter: (req, res) => {
+                    if (req.headers["x-no-compression"]) {
+                        return false; // Bypass compression if the client requests it
+                    }
+                    return compression_1.default.filter(req, res); // Use default filter
+                },
+            }));
         });
     }
     catch (err) {
--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,9 @@ import express from "express";
 import pool from "./db/postgres";
 import authRoutes from "./routes/authRoutes";
 import stockRoutes from "./routes/StockRoutes";
-// import compression from "compression"; // Import the compression middleware
+import compression from "compression"; // Import the compression middleware
 import { authenticateJWT, apiLimiter } from "./middleware";
+import cors from "cors";
 
 import logger from "./logger";
 
@@ -12,7 +13,13 @@ const port = 3000;
 const hostName = "0.0.0.0";
 
 app.use(express.json());
-
+app.use(
+  cors({
+    origin: "*", // Allow all origins (change this for security)
+    methods: "GET,POST,PUT,DELETE",
+    allowedHeaders: "Content-Type, Authorization",
+  })
+);
 app.use("/api", apiLimiter); // Apply rate limiter to all /api routes
 
 app.get("/api/ping", (req, res) => {
@@ -31,16 +38,16 @@ const startServer = async () => {
       // Apply JWT middleware and prefix /private to stockRoutes
       app.use("/api/private", authenticateJWT, stockRoutes);
 
-      // app.use(
-      //   compression({
-      //     filter: (req, res) => {
-      //       if (req.headers["x-no-compression"]) {
-      //         return false; // Bypass compression if the client requests it
-      //       }
-      //       return compression.filter(req, res); // Use default filter
-      //     },
-      //   })
-      // );
+      app.use(
+        compression({
+          filter: (req, res) => {
+            if (req.headers["x-no-compression"]) {
+              return false; // Bypass compression if the client requests it
+            }
+            return compression.filter(req, res); // Use default filter
+          },
+        })
+      );
     });
   } catch (err) {
     logger.error("Failed to connect to the database", err);
